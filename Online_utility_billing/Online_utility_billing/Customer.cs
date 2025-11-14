224371d7fd6f6eebbc0e559d3d32803377ff66e5
Dharanisri Ravi <dharanisrir@infinite.com>
2025-11-14 17:32:47 +0530

    Initial commit

---
+++ Online_utility_billing/Online_utility_billing/Customer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Online_utility_billing
+{
+    internal class Customer
+    {
+
+        int customer_ID;
+        string customer_Name;
+        int total_readings;
+        double total_usage;
+        double tax;
+
+        public Customer(int customer_ID, string customer_Name)
+        {
+            this.customer_ID = customer_ID; 
+            this.customer_Name = customer_Name;
+
+        }
+        public void GetReadings(params int[] readings )
+        {
+            total_readings = 0;
+            foreach (int read in readings)
+            {
+                total_readings += read;
+            }
+
+        }
+        public void CalculateGST()
+        {
+            total_usage = total_readings * 6.5;
+            tax=total_usage* 18/ 100;
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine("=======CUSTOMER BILL DETAILS======");
+            Console.WriteLine($"\nID: {customer_ID}");
+            Console.WriteLine($"\nName: {customer_Name}");
+            Console.WriteLine("\nService Charge: Rs.50");
+            //Console.WriteLine($"\n Tax Applied : {tax}");
+            Console.WriteLine($"\nTotal usage:{total_readings * 6.5}");
+            Console.WriteLine($"\nTax Applied : {tax}");
+            Console.WriteLine($"\nNet Payable: {50+tax+total_readings} units");
+            Console.WriteLine("\n---------------------------------------------------");
+
+        }
+
+    }
+}
---
+++ Online_utility_billing/Online_utility_billing/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_utility_billing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=======Welcome to Online Utility Billing System======");
+           
+
+            Console.WriteLine("\nEnter the no of customers");
+            int no_of_cust=Convert.ToInt32(Console.ReadLine());
+            Customer[] customer = new Customer[no_of_cust];
+
+            for (int i=0;i<no_of_cust;i++)
+            {
+                Console.WriteLine("\n---------------------------------------------------");
+                Console.WriteLine($"\nEnter details for customer {i + 1}:");
+                Console.WriteLine("\n\"Enter ID: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\n\"Enter Name: ");
+                string name= Console.ReadLine();
+                customer[i]=new Customer(id, name);
+                Console.Write("\nEnter number of readings: ");
+                int count = Convert.ToInt32(Console.ReadLine());
+                int [] readings = new int[count];
+                for (int j = 0; j < count; j++)
+                {
+                    Console.Write($"\nEnter reading {j + 1}: ");
+                    readings[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                customer[i].GetReadings(readings);
+                 customer[i].CalculateGST();
+                Console.WriteLine("\n---------------------------------------------------");
+
+                customer[i].DisplayDetails();
+
+
+            }
+        }
+    }
+}
